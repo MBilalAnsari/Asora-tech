7d984630f1a8f672d4f766c27ec428912105c603
MBilalAnsari <m.bilal.ansari.qadri.786@gmail.com>
2025-12-03 16:32:44 +0500

    update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,9 +6,6 @@ export const signup = async (req, res) => {
   const { username, email, phone, password } = req.body;
 
   try {
-    console.log("Creating user:", { username, email, phone });
-
-    
   
     const exists = await User.findOne({ email });
     if (exists) return res.status(400).json({ message: "User already exists" });
@@ -16,11 +13,11 @@ export const signup = async (req, res) => {
 
     const hashed = await bcrypt.hash(password, 10);
 
-     // Generate JWT token
-    const token = generateToken(user._id);
-
     // Create user
     const user = await User.create({ username, email, phone, password: hashed });
+
+    // Generate JWT token
+    const token = generateToken(user._id);
 
     res.status(201).json({ message: "User created successfully", token });
   } catch (error) {
@@ -31,10 +28,10 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
+
   try {
    
     const user = await User.findOne({ email });
-    console.log("Logging in user:", email);
     if (!user) return res.status(404).json({ message: "User not found" });
 
     // Compare password
